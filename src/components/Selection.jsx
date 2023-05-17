1705eeb301cd64dcfe4f89ea84415d65f1c050c8
Verity Gregory <verity.gregory@gmail.com>
2023-05-17 10:18:13 +0100

    added key to map

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -2,9 +2,10 @@ export const Selection = ({ dice, rolled }) => {
   return (
     <>
       <div className="dice-pool">
-        {dice.map((die) => {
+        {dice.map((die, index) => {
+          console.log(die);
           return (
-            <div className="rolled">
+            <div className="rolled" key={index}>
               <img src={die.img} className="dice-pool-dice" />
               {rolled ? <h3 className="result">{die.dice}</h3> : null}
             </div>
